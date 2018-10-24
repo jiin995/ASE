5ef758ada24a74182efde496de003759c349cec2
jiin995 <gabrieleprevitera@gmail.com>
2018-10-24 23:59:43 +0200

    aggiunto half_adder

---
+++ GHDL/adder/half_adder/half_adder.vhd
@@ -0,0 +1,24 @@
+-------------------------------------------------------
+--! @file
+--! @brief half_adder with only 2 input e 2 output, carry and sum
+-------------------------------------------------------
+-- half_adder.vhd describes an simple half_adder
+-- Author : Gabriele Previtera
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+--define component and declare its interface
+entity half_adder is
+  PORT (  X:  in  STD_LOGIC;
+          Y:  in  STD_LOGIC;
+          C:  out STD_LOGIC;
+          S:  out STD_LOGIC
+  );
+end half_adder;
+
+architecture dataflow of half_adder is
+  begin
+    C <=  (X and Y);
+    S <=  (X xor Y);
+end dataflow;
---
+++ GHDL/adder/half_adder/half_adder_testbench.vhd
@@ -0,0 +1,39 @@
+-- Definition of testbench for half_adder
+-- Author: Gabriele Previtera
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity half_adder_testbench is
+end half_adder_testbench;
+
+architecture behavioral of half_adder_testbench is
+
+  component half_adder is
+    PORT (  X : in  STD_LOGIC;
+            Y : in  STD_LOGIC;
+            C : out STD_LOGIC;
+            S : out STD_LOGIC
+    );
+  end component;
+
+  signal  X  :  STD_LOGIC :='0';
+  signal  Y  :  STD_LOGIC :='0';
+
+  signal  C  :  STD_LOGIC :='0';
+  signal  S  :  STD_LOGIC :='0';
+
+  begin
+    uuit: half_adder PORT MAP ( X =>  X,
+                                Y =>  Y,
+                                C =>  C,
+                                S =>  S
+                              );
+
+    stim_proc: process
+      begin
+        X <= '1','0' after 15 ns,'1' after 25 ns,'1' after 35 ns;
+        Y <= '0','1' after 15 ns,'1' after 25 ns,'1' after 35 ns;
+        wait;
+    end process;
+end behavioral;
