3f2ebcbc19c53f2efeafe9122680644b8fead03a
jiin995 <gabrieleprevitera@gmail.com>
2018-11-09 00:11:00 +0100

    aggiunto script per generare documentazione

---
+++ GHDL/mux/mux2_1/gen_doc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR_NAME=${PWD##*/}
+DOXYGEN_FILES="/home/jiin995/ASE_WorkSpace/Doxygen"
+
+if [ $1 == "clear" ]; then 
+    rm -dr latex
+    #rm -dr html
+    rm -f Doxyfile
+    exit
+fi
+
+if [ ! -f Doxyfile ]; then 
+    echo "Doxyfile not exist"
+    doxygen -g 
+    sed -i "s/PROJECT_NAME           = \"My Project\"/PROJECT_NAME = \"$DIR_NAME\"/g" Doxyfile
+    sed -i "s|LATEX_HEADER           =|LATEX_HEADER           = \"$DOXYGEN_FILES/header.tex\"|g" Doxyfile
+    sed -i "s|GENERATE_HTML          = YES|GENERATE_HTML          = NO|g" Doxyfile
+    
+fi
+
+doxygen Doxyfile
+cd latex && make pdf && cd ../
