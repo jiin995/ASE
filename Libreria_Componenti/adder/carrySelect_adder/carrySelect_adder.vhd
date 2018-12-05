d68d623950036fe6238419034d22636bad907639
jiin995 <gabrieleprevitera@gmail.com>
2018-12-05 01:10:55 +0100

    Completato carry select e testato, si deve commentare e aggiunger e doxygen

--- Libreria_Componenti/adder/carrySelect_adder/carrySelect_adder.vhd
+++ Libreria_Componenti/adder/carrySelect_adder/carrySelect_adder.vhd
@@ -2,17 +2,21 @@ library IEEE;
     use IEEE.STD_LOGIC_1164.all;
 
 entity carrySelect_adder is 
-    generic (   width   : NATURAL := 8
+    generic (   width       : NATURAL := 8;
+    -- M parallelismo dei ripplecarry adder
+                M    : NATURAL := 4;
+    -- P parallelismo delle celle dell carry select
+                P        : NATURAL := 2
     -- Come metto M e P, marco e co fanno la stima dei tempi e mettono solo width da cui ricavano poi M e P 
     -- io direi di fare una versione con M e P espliciti e una versione come l'hanno fatta loro, ma su quella
     --resta da capire come fanno a prendere solo una parte del segnale 
     );
     port (
-            A       : in    STD_LOGIC_VECTOR ((M*P-1) downto 0);
-            B       : in    STD_LOGIC_VECTOR ((M*P-1) downto 0);
-            c_in    : in    STD_LOGIC ;
-            S       : out   STD_LOGIC_VECTOR ((M*P-1) downto 0);
-            c_out   : out   STD_LOGIC
+            A       : in    STD_LOGIC_VECTOR (((M*P)-1) downto 0);  -- input addendo
+            B       : in    STD_LOGIC_VECTOR (((M*P)-1) downto 0);  -- input addendo
+            c_in    : in    STD_LOGIC ;                             -- input carry in ingresso
+            S       : out   STD_LOGIC_VECTOR (((M*P)-1) downto 0);  -- output somma
+            c_out   : out   STD_LOGIC                               -- output carry in uscita
     );
 end carrySelect_adder;
 
@@ -22,8 +26,44 @@ end carrySelect_adder;
 
 architecture structural of carrySelect_adder is
 
+    component carrySelect_cell is 
+        generic (   width   : NATURAL := M
+        );
+        port (
+                A       : in    STD_LOGIC_VECTOR ((width-1) downto 0);
+                B       : in    STD_LOGIC_VECTOR ((width-1) downto 0);
+                c_in    : in    STD_LOGIC ;
+                S       : out   STD_LOGIC_VECTOR ((width-1) downto 0);
+                c_out   : out   STD_LOGIC
+        );
+    end component;
 
+signal internal_carry : STD_LOGIC_VECTOR ((P) downto 0) := (others => '0');
+
+--type arrays_of_VECTOR is array (0 to P-1) of STD_LOGIC_VECTOR ((M-1) downto 0);
+--signal internal_sums : arrays_of_VECTOR; 
+
+signal S_TEMP : STD_LOGIC_VECTOR (((M*P)-1) downto 0 ) := (others => '0');
 
 begin
-    c_out <= c_in;
+
+    S <= S_TEMP;
+
+    -- collego i segnali temporanei dei carry, con i porti esterni del componenti 
+    -- in modo tale da non mettere if nel for generate
+    c_out <= internal_carry(P);
+    internal_carry(0) <= c_in; 
+
+-- istanzio il numero di celle e le mappo con gli ingressi
+    cells:
+        for i in 0 to P-1 generate 
+            cell: carrySelect_cell port map (   A       => A ((((i+1)*M)-1) downto (i*M)),
+                                                B       => B ((((i+1)*M)-1) downto (i*M)),
+                                                c_in    => internal_carry(i),
+                                                S       => S_TEMP((((i+1)*M)-1) downto (i*M)),
+                                                c_out   => internal_carry(i+1)
+                            
+            );
+        end generate cells;
+
 end structural;
---
+++ Libreria_Componenti/adder/carrySelect_adder/carrySelect_adder_testbench.vhd
@@ -0,0 +1,59 @@
+library IEEE;
+    use IEEE.STD_LOGIC_1164.all;
+    use ieee.numeric_std.all;
+
+entity carrySelect_adder_testbench is 
+    generic ( M_test : NATURAL := 4;
+              P_test : NATURAL :=4
+    );
+end carrySelect_adder_testbench;
+
+architecture behavioral of carrySelect_adder_testbench is 
+    component carrySelect_adder is 
+        generic (   width       : NATURAL := 8;
+                    M    : NATURAL := M_test;
+                    P        : NATURAL := P_test
+        );
+        port (
+                A       : in    STD_LOGIC_VECTOR ((M*P-1) downto 0);
+                B       : in    STD_LOGIC_VECTOR ((M*P-1) downto 0);
+                c_in    : in    STD_LOGIC ;
+                S       : out   STD_LOGIC_VECTOR ((M*P-1) downto 0);
+                c_out   : out   STD_LOGIC
+        );
+    end component;
+
+signal X        : STD_LOGIC_VECTOR ((M_test*P_test)-1 downto 0) := x"0012";
+signal Y        : STD_LOGIC_VECTOR ((M_test*P_test)-1 downto 0) := x"0002";
+signal S        : STD_LOGIC_VECTOR ((M_test*P_test)-1 downto 0) := (others => '0');
+signal c_in     : STD_LOGIC :='0';
+signal c_out    : STD_LOGIC :='0';
+
+begin
+
+    uut: carrySelect_adder port map (   A => X,
+                                        B => Y,
+                                        c_in => c_in,
+                                        c_out => c_out,
+                                        S => S
+
+    );
+
+    stim_proc :process 
+        begin  
+            wait  for 10 ns;
+            for i in 0 to 10 loop 
+                X <= std_logic_vector(to_unsigned(to_integer(unsigned( X )) + 5, (M_test*P_test)));
+                wait for 10 ns;
+            end loop;
+            -- testo la variazione dell carry
+            c_in <= '1';
+            wait for 20 ns;
+
+            -- testo il c_out
+            Y <= x"FFFF";
+            wait for 20 ns;
+            wait;
+        end process;
+
+end behavioral;
