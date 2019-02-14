faeb71ba49bd09db5b6b0551f2678a05bda0741d
Mirko Pennone <mirko@pennone.org>
2019-02-14 17:15:50 +0100

    tatta

--- Esercitazioni/Esercitazione_6/fsm_mealy/fsm_mealy.vhd
+++ Esercitazioni/Esercitazione_6/fsm_mealy/fsm_mealy.vhd
@@ -1,33 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:48:56 02/14/2019 
--- Design Name: 
--- Module Name:    fsm_mealy - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity fsm_mealy is
     Port ( clock : in  BIT;
@@ -37,51 +9,53 @@ end fsm_mealy;
 
 architecture singular_state_machine of fsm_mealy is
 
-	type state is (init, got1, got10, got101, got1011, got10110, got101100);
+	type state is (init, s1, s10, s101, s1011, s10110, s101100);	-- definisco tipo stato
+	type state_vector is array(natural range<>) of state;			-- definisco vettore di stati
 	
-	type state_vector is array(natural range<>) of state;
-	function one_of (source: state_vector) return state is
+	function one_of (source: state_vector) return state is			-- funzione di risoluzione: viene invocata quando
+																	-- occore un evento su uno qualsiasi degli input per
+																	-- il segnale a cui è applicata
 	begin
-		return source(source'left);
+		return source(source'left);		-- ritorna il primo stato di state_vector
 	end one_of;
 	
-	signal current: one_of state register := init;
+	signal current_state: one_of state register := init;	-- stato corrente, viene definito con la funzione di risoluzione
 	
 	begin
-	clocking: block(clock='1' and not clock'stable)
+	clocking: block (clock='1' and not clock'stable)			-- condizione per la guardia
 		begin
-			q0: block (current=init and guard)
+			q0: block (current_state=init and guard)			-- guardia innestata
 			begin
-				current<= guarded got1 when x='1' else init;
+				current_state<= guarded s1 when x='1' else init;
 			end block q0;
-			q1: block (current=got1 and guard)
+			q1: block (current_state=s1 and guard)
 			begin
-				current<= guarded got10 when x='0' else got1;
+				current_state<= guarded s10 when x='0' else s1;
 			end block q1;
-			q2: block (current=got10 and guard)
+			q2: block (current_state=s10 and guard)
 			begin
-				current<= guarded got101 when x='1' else init;
+				current_state<= guarded s101 when x='1' else init;
 			end block q2;
 	
-			q3: block (current=got101 and guard)
+			q3: block (current_state=s101 and guard)
 			begin
-				current<= guarded got1011 when x='1' else got10;
+				current_state<= guarded s1011 when x='1' else s10;
 			end block q3;
 			
-			q4: block (current=got1011 and guard)
+			q4: block (current_state=s1011 and guard)
 			begin
-				current<= guarded got10110 when x='0' else got1;
+				current_state<= guarded s10110 when x='0' else s1;
 			end block q4;
 			
-			q5: block (current= got10110 and guard)
+			q5: block (current_state= s10110 and guard)
 			begin
-				current<= guarded got101100 when x='0' else got1;
+				current_state<= guarded s101100 when x='0' else s1;
 			end block q5;
 			
-			q6: block (current=got101100 and guard)
+			q6: block (current_state=s101100 and guard)
 			begin
-				current<= guarded init;
-				y<= '1' when x='1' and current=got101100 else '0';
+				current_state<= guarded init;
+				y<= guarded '1' when x='1' and current_state=s101100 else '0';	-- l'uscita è alta quando sono in s101100 e rilevo x=1 (Mealy)!
 			end block q6;
 			
 	end block clocking;
