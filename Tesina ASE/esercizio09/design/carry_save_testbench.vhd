ec42aebf7895a6022bb051a31999685dfc74c6f9
Mirko Pennone <mirko@pennone.org>
2019-02-16 18:55:09 +0100

    quasi finito es 9

---
+++ Tesina ASE/esercizio09/design/carry_save_testbench.vhd
@@ -0,0 +1,100 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   12:58:02 01/29/2018
+-- Design Name:   
+-- Module Name:   C:/Documents and Settings/OK/carry_save/carry_save_testbench.vhd
+-- Project Name:  carry_save
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: carry_save
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY carry_save_testbench IS
+END carry_save_testbench;
+ 
+ARCHITECTURE behavior OF carry_save_testbench IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT carry_save
+    PORT(
+         X : IN  std_logic_vector(3 downto 0);
+         Y : IN  std_logic_vector(3 downto 0);
+         Z : IN  std_logic_vector(3 downto 0);
+         S : OUT  std_logic_vector(5 downto 0)
+        );
+    END COMPONENT;
+
+   --Inputs
+   signal X : std_logic_vector(3 downto 0) := (others => '0');
+   signal Y : std_logic_vector(3 downto 0) := (others => '0');
+   signal Z : std_logic_vector(3 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal S : std_logic_vector(5 downto 0);
+
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: carry_save PORT MAP (
+          X => X,
+          Y => Y,
+          Z => Z,
+          S => S
+        );
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+		
+		X <= x"4";
+		Y <= x"6";
+		Z <= x"7";
+		
+		wait for 30 ns;
+		
+		X <= x"E";
+		Y <= x"9";
+		
+		wait for 30 ns;
+		
+		Y <= x"9";
+		Z <= x"8";
+		
+		wait for 30 ns;
+		
+		Z <= x"7";
+		X <= x"8";
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
---
+++ Tesina ASE/esercizio09/design/carry_save_timing.vhd
@@ -0,0 +1,113 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:00:52 02/16/2019 
+-- Design Name: 
+-- Module Name:    carry_save_timing - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity carry_save_timing is
+	generic (width : natural := 128);
+    Port ( clock : in  STD_LOGIC;
+           X : in  STD_LOGIC_VECTOR (width-1 downto 0);
+           Y : in  STD_LOGIC_VECTOR (width-1 downto 0);
+			  Z : in  STD_LOGIC_VECTOR (width-1 downto 0);
+           S : out  STD_LOGIC_VECTOR (width+1 downto 0)
+			  );
+end carry_save_timing;
+
+architecture Behavioral of carry_save_timing is
+
+	component carry_save is 
+    generic (   width     :   NATURAL := width
+    );
+    port (  
+           X : in  STD_LOGIC_VECTOR (width-1 downto 0);
+           Y : in  STD_LOGIC_VECTOR (width-1 downto 0);
+			  Z : in  STD_LOGIC_VECTOR (width-1 downto 0);
+           S : out  STD_LOGIC_VECTOR (width+1 downto 0)
+    );
+	end component;
+
+	component d_edge_in is
+		generic(width : natural := width);
+		port(
+				clock: in STD_LOGIC;
+				D:	in STD_LOGIC_VECTOR(width-1 downto 0);
+				Q:	out STD_LOGIC_VECTOR(width-1 downto 0));
+	end component;
+	
+	component d_edge_out is
+		generic(width : natural := width+2);
+		port(
+				clock: in STD_LOGIC;
+				D:	in STD_LOGIC_VECTOR(width-1 downto 0);
+				Q:	out STD_LOGIC_VECTOR(width-1 downto 0));
+	end component;
+	
+	signal my_a : std_logic_vector(width-1 downto 0);
+	signal my_b : std_logic_vector(width-1 downto 0);
+	signal my_c : std_logic_vector(width-1 downto 0);
+	signal my_sum : std_logic_vector(width+1 downto 0);
+
+
+begin
+	
+
+	Load_a: d_edge_in PORT MAP(
+		D => X,
+		clock => clock,
+		Q => my_a
+	);
+	
+	Load_b: d_edge_in PORT MAP(
+		D => Y,
+		clock => clock,
+		Q => my_b
+	);
+	
+	Load_c: d_edge_in PORT MAP(
+		D => Z,
+		clock => clock,
+		Q => my_c
+	);
+	
+	Load_sum: d_edge_out PORT MAP(
+		D => my_sum,
+		clock => clock,
+		Q => S
+	);
+
+	Inst_carry_save: carry_save PORT MAP(
+		X => my_a,
+		Y => my_b,
+		Z => my_c,
+		S => my_sum
+	);
+	
+
+end Behavioral;
+
