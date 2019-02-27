aa630aef898efffe6ec2858efe7e0bea7fc4b428
jiin995 <gabrieleprevitera@gmail.com>
2019-02-27 23:12:42 +0100

    Rimosso cose inutile e funziona

--- Libreria_Componenti/uart/uart_po_pc/uart_tx.vhd
+++ Libreria_Componenti/uart/uart_po_pc/uart_tx.vhd
@@ -26,21 +26,18 @@ architecture Behavioral of uart_tx is
 	component uart_tx_cu is 
 		 port (	clock   				: in  STD_LOGIC;
 					reset   				: in  STD_LOGIC;
-					tx_start				: in  STD_LOGIC;
-					tick_hit    		: in  STD_LOGIC;    -- segnale dal bound rate gen 
-					stop_hit				: in 	STD_LOGIC;
-					sended_byte_hit	: in  STD_LOGIC;
-					tx_in					: in  STD_LOGIC;
-					load_din				: out STD_LOGIC;
-					enable_stop			: out STD_LOGIC;
-					enable_tick			: out STD_LOGIC;
-					increase_n_bits 	: out STD_LOGIC;
-					send_tx				: out STD_LOGIC;
-					reset_out 			: out STD_LOGIC;
-					reset_scan			: out STD_LOGIC;
-					tx_out				: out STD_LOGIC;
-					reset_tick			: out  STD_LOGIC;
-					tx_done 				: out STD_LOGIC
+					tx_start				: in  STD_LOGIC;		-- segnale che indica che la trasmissione deve iniziare	
+					tick_hit    		: in  STD_LOGIC;    	-- raggiungimento dei 16 conteggi per inviare 
+					stop_hit				: in 	STD_LOGIC;		-- hit dello stop counter
+					sended_byte_hit	: in  STD_LOGIC;		-- alto quando ho inviato un byte
+					bit_to_send			: in  STD_LOGIC;		-- bit in ingresso da po , è l'uscita della scan chain ed è il bit da inviare 
+					load_din				: out STD_LOGIC;		--	segnala alla scan chain di caricare il byte da inviare
+					increase_n_bits 	: out STD_LOGIC;		-- incrementa il contatore del numero di bit inviati
+					send_tx				: out STD_LOGIC;		-- indica alla scan chain di eseguire uno shif, il bit in uscita sarà il prossimo ad essere inviato
+					reset_out 			: out STD_LOGIC;		-- resetta i contatori tranne il tick counter
+					tx_out				: out STD_LOGIC;		-- uscita tx dalla cu, perchè la cu in baso allo stato deve decidere se tx deve essere alta, bassa o inviare il bit 
+					reset_tick			: out STD_LOGIC;		-- resetta il tick counter
+					tx_done 				: out STD_LOGIC		-- segnala la fine dell'invio
 			);
 	end component;
 
@@ -50,33 +47,29 @@ architecture Behavioral of uart_tx is
 						start_Ticks	: NATURAL := 7;
 						stop_Ticks  : NATURAL := 16		-- Numero di conteggi per determinare la fine della trasmissione
 		  );	
-		port		(	clock					: in 	STD_LOGIC;
+		 port		(	clock					: in 	STD_LOGIC;
 						reset					: in 	STD_LOGIC;
 						enable				: in 	STD_LOGIC;
-						enable_stop			: in  STD_LOGIC;
-						enable_tick			: in  STD_LOGIC;
-						sended_bit			: in  STD_LOGIC;
-						send_tx				: in  STD_LOGIC;
-						tx_done				: in  STD_LOGIC;
-						wr_uart				: in  STD_LOGIC;
-						tx_mark				: in  STD_LOGIC;
-						tx_in					: in  STD_LOGIC;
-						load_din				: in  STD_LOGIC;
-						reset_tick			: in  STD_LOGIC;
-						byte_to_send		: in 	STD_LOGIC_VECTOR ( ( data_bits-1 ) downto 0);
-						tx						: out STD_LOGIC;
-						tx_out				: out STD_LOGIC;
-						tx_full				: out STD_LOGIC;
-						sended_byte_hit	: out STD_LOGIC;
-						tick_hit				: out STD_LOGIC;
-						reset_scan			: in STD_LOGIC;
-						stop_hit				: out STD_LOGIC
+						sended_bit			: in  STD_LOGIC;	-- quando va alto, viene incrementato il contatore del numero di bit inviati	
+						send_tx				: in  STD_LOGIC;	-- quando va alto si esegue uno shift nella scan chain
+						tx_done				: in  STD_LOGIC;	-- quando va alto si aggiorna lo stato del buffer, segnala la fine della trasmissione, il buffer è svuotato
+						wr_uart				: in  STD_LOGIC;	-- quando va alto si idica alla parte di trasmissione che si vuole inviare il byte che si trova su byte_to_send
+						tx_in					: in  STD_LOGIC;	-- tx in ingresso dalla pc ed è quello che deve essere inviato su tx
+						load_din				: in  STD_LOGIC;	--	scan en della scan chain, quando è basso carica il byte nella scan chain
+						reset_tick			: in  STD_LOGIC;	-- resetta il tick counter
+						byte_to_send		: in 	STD_LOGIC_VECTOR ( ( data_bits-1 ) downto 0); 	-- byte da inviare
+						tx						: out STD_LOGIC;	-- linea in uscita
+						bit_to_send			: out STD_LOGIC;	-- bit da invare , viene trasmesso al pc 
+						tx_full				: out STD_LOGIC;	-- va alto quando il buffer per inviare è pieno, va basso quando viene svuotato
+						sended_byte_hit	: out STD_LOGIC;	-- va alto quando è stato inviato tutto il byte
+						tick_hit				: out STD_LOGIC;	-- va alto quando si raggiungono i 16 conteggi del tick counter
+						stop_hit				: out STD_LOGIC	-- va alto quando si raggiungono i conteggi dello stop counter 
 						
 		);
 	end component;
 	
-	signal enable_stop, enable_tick,sended_bit, send_tx, tx_done, tx_mark, reset_po, reset_scan,
-				sended_byte_hit, tick_hit, stop_hit, tx_full_int, reset_out, load_din,tx_in,tx_out,reset_tick: STD_LOGIC := '0';
+	signal sended_bit, send_tx, tx_done, reset_po,sended_byte_hit, tick_hit,
+		stop_hit, tx_full_int, reset_out, load_din, tx_in_po, tx_out_po,reset_tick: STD_LOGIC := '0';
 
 begin
 	tx_full <= tx_full_int;
@@ -90,22 +83,18 @@ begin
 						port		map(	clock					=> clock,
 											reset					=> reset_po,
 											enable				=> enable,
-											enable_stop			=> enable_stop,
-											enable_tick			=> enable_tick,
 											sended_bit			=> sended_bit,
 											send_tx				=> send_tx,
 											tx_done				=> tx_done,
 											wr_uart				=> wr_uart,
 											tx						=> tx,
-											tx_mark				=> tx_mark,
 											load_din				=> load_din,
 											byte_to_send		=> din,
 											tx_full				=> tx_full_int,
 											sended_byte_hit	=> sended_byte_hit,
-											tx_in					=> tx_in,
-											tx_out				=> tx_out,
+											tx_in					=> tx_in_po,
+											bit_to_send			=> tx_out_po,
 											tick_hit				=> tick_hit,
-											reset_scan			=>	reset_scan,
 											reset_tick			=> reset_tick,
 											stop_hit				=> stop_hit						
 							);
@@ -117,15 +106,12 @@ begin
 											tick_hit    		=> tick_hit,
 											stop_hit				=> stop_hit,
 											sended_byte_hit	=> sended_byte_hit,
-											enable_stop			=> enable_stop,
-											enable_tick			=> enable_tick,
 											increase_n_bits 	=> sended_bit,
 											load_din				=> load_din,
 											send_tx				=> send_tx,
 											reset_out 			=> reset_out,
-											tx_in					=> tx_out,
-											tx_out				=> tx_in,
-											reset_scan			=> reset_scan,
+											bit_to_send			=> tx_out_po,
+											tx_out				=> tx_in_po,
 											reset_tick			=> reset_tick,
 											tx_done 				=> tx_done
 									);
--- Libreria_Componenti/uart/uart_po_pc/uart_tx_cu.vhd
+++ Libreria_Componenti/uart/uart_po_pc/uart_tx_cu.vhd
@@ -12,14 +12,11 @@ entity uart_tx_cu is
             tick_hit    		: in  STD_LOGIC;    -- segnale dal bound rate gen 
 				stop_hit				: in 	STD_LOGIC;
 				sended_byte_hit	: in  STD_LOGIC;
-				tx_in					: in 	STD_LOGIC;	
-				enable_stop			: out STD_LOGIC;
-				enable_tick			: out STD_LOGIC;
+				bit_to_send					: in 	STD_LOGIC;	
 				increase_n_bits 	: out STD_LOGIC;
 				send_tx				: out STD_LOGIC;
 				load_din				: out STD_LOGIC;		-- 0 attivo carica nella scan chain din
 				reset_out 			: out STD_LOGIC;
-				reset_scan			: out STD_LOGIC;
 				tx_out				: out STD_LOGIC;
 				reset_tick			: out STD_LOGIC;
             tx_done 				: out STD_LOGIC
@@ -47,60 +44,51 @@ begin
    --se non ci sono variazioni rimane nella configurazione precedente
 	state_next 			<= current_state;
 	tx_done				<= '0';
-	enable_stop 		<= '0';
 	increase_n_bits 	<= '0';
 	send_tx				<= '0';
 	load_din				<= '1';
 	reset_out 			<= '0';
-	reset_scan			<= '0';
-   tx_done 				<= '0';
-	tx_out 				<= '1';
-	enable_tick			<= '1';
+	tx_out 				<= '1';						-- la line a riposo deve essere alta
 	reset_tick			<= '0';
 	
 	case (current_state ) is 
 		when idle =>
-			load_din	<= '0'; 	-- carico nella scan chain il byte da inviare
-			enable_stop <= '1';
-			reset_tick <= '1';
+			load_din		<= '0'; 						-- carico nella scan chain il byte da inviare
+			reset_tick 	<= '1';						-- resetto il tick counter
 			if tx_start = '1' then					-- inizio la trasmissione quando il dato è fissato nel buffer
-				tx_out 	<= '0';
+				tx_out 	<= '0';						-- abbasso la linea di trasmissione
 				state_next 	<= start;
 			end if;
 			
 		when start =>	
 			-- abbasso la linea per segnalare al ricevente l'inizio della trasmissione
-			tx_out 		<= '0';
-			enable_tick	<= '1';
-			load_din		<= '0';
-			send_tx		<= '1';	-- clock della scan chain
-			reset_out	<= '1';
+			tx_out 		<= '0';						-- abbasso la linea di trasmissione
+			load_din		<= '0';						-- carico nella scan chain il byte da inviare
+			send_tx		<= '1';						-- clock della scan chain
+			reset_out	<= '1';						-- resetto i contatori tranne il tick counter
+			-- aspetto per poter iniziare la trasmissione
 			if ( tick_hit = '1' ) then 
-				-- aspetto per poter iniziare la trasmissione
 				state_next	<= send;
-
 			end if;
 		
 		when send => 
 			-- invio il LSb che ancora devo inviare
-			tx_out <= tx_in;
-			enable_tick	<= '1';
+			tx_out 		<= bit_to_send;					-- invio il primo bit sulla linea
 			if ( tick_hit = '1' ) then 
-				send_tx				<= '1';
-				increase_n_bits 	<= '1';
+				send_tx				<= '1';			-- faccio shiftare la scain chain e scan out è prossimo bit da inviare 
+				increase_n_bits 	<= '1';			-- incremento il contatore dei bit inviati
 				if ( sended_byte_hit = '1' ) then 
-					state_next <= stop ;
+					state_next 		<= stop ;
 				end if;
 			end if;
 			
 		-- stato finale devo aspettare un certo numero di tick per terminare portarmi nello stato di idle
 		when stop => 
-			enable_stop <= '1';
-			enable_tick	<= '1';
-			reset_tick <= '1';
+			reset_tick 	<= '1';						-- resetto il tick count per poter iniziare subito una nuova trasmissione
 			if ( stop_hit = '1' ) then 
 				state_next 	<= idle;
-				tx_done 		<= '1';
+				reset_out 	<= '1';					-- resetto i contatori
+				tx_done 		<= '1';					-- segnalo la fine dell'invio
 			end if;
 			
 	end case;
